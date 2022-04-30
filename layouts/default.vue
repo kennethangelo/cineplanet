93dfc0fac56cf762b76fcd47aed6b6ba97674cdb
Kenneth Angelo <tanjayakenneth@gmail.com>
2022-05-01 00:01:39 +0700

    finish

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="app">
+  <Nuxt keep-alive />
+</div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      movies: [],
+       
+    }
+  },
+  methods:{
+    async getMovies(){
+      const data = axios.get()
+    }
+  }
+}
+</script>
